ba607ec4f0ec2cf2835059f7c2307482539af21e
Mateus <166668055+L0L0VIS@users.noreply.github.com>
2025-03-28 16:45:43 -0300

    ArquivoSerie.java

--- arquivos/ArquivoSerie.java
+++ arquivos/ArquivoSerie.java
@@ -1,75 +1,81 @@
 package arquivos;
 
+import java.io.IOException;
+import java.io.RandomAccessFile;
 
 import Entidades.Serie;
 import entidade.ParNomeID;
 import estruturas.HashExtensivel;
 
-public class ArquivoSerie extends Arquivo<Serie>
-{
+public class ArquivoSerie extends Arquivo<Serie> {
 
-    Arquivo<Serie> arqSerie;
-    HashExtensivel<ParNomeID> indiceIndiretoNome;
+    private HashExtensivel<ParNomeID> indiceIndiretoNome;
 
-    //@Override
     public ArquivoSerie() throws Exception {
         super("series", Serie.class.getConstructor());
         indiceIndiretoNome = new HashExtensivel<>(
             ParNomeID.class.getConstructor(), 
             4, 
             ".\\dados\\series\\indiceNome.d.db",   // diretório
-            ".\\dados\\series\\indiceNome.c.db"    // cestos 
+            ".\\dados\\series\\indiceNome.c.db"    // cestos
         );
     }
 
     @Override
     public int create(Serie c) throws Exception {
-        //System.out.println("ArquivoSerie super being formed");
+        if (read(c.getNome()) != null) {
+            System.out.println("Série já cadastrada: " + c.getNome());
+            return -1; // Retorna um valor inválido se a série já existir
+        }
         int id = super.create(c);
-        //System.out.println("ArquivoSerie super formed");
-        //System.out.println("Creating ParNomeID of id: " + id + " to Serie: " + c.getNome());
-        //System.out.println("ArquivoSerie IndiceIndireto being formed");
         indiceIndiretoNome.create(new ParNomeID(c.getNome(), id));
-        //System.out.println("ArquivoSerie IndiceIndireto formed");
         return id;
+    }
+
+    public boolean temSeries() {
+        try (RandomAccessFile raf = new RandomAccessFile(nomeArquivo, "r")) {
+            return raf.length() > 4; // Deve ter pelo menos 4 bytes para armazenar algum dado
+        } catch (IOException e) {
+            System.out.println("Erro ao acessar o arquivo de séries: " + e.getMessage());
+        }
+        return false;
     }
 
     public Serie read(String nome) throws Exception {
         ParNomeID pni = indiceIndiretoNome.read(ParNomeID.hash(nome));
-        //System.out.println("PNomeID encontrado: " + pni.getId() + " " + pni.getnome());
-        if(pni == null)
-            return null;
+        if (pni == null) return null;
         return read(pni.getId());
     }
     
     public boolean delete(String nome) throws Exception {
         ParNomeID pni = indiceIndiretoNome.read(ParNomeID.hash(nome));
-        //System.out.println("PNomeID encontrado para deletar: " + pni.getId() + " " + pni.getnome());
-        if(pni != null) 
-            if(delete(pni.getId())) 
-                //return indiceIndiretoNome.delete(ParNomeID.hash(nome));
-                return true;
-                // Antigo return substituído pois era redundante com o outro método delete
+        if (pni != null && delete(pni.getId())) {
+            return indiceIndiretoNome.delete(ParNomeID.hash(nome));
+        }
         return false;
     }
 
     @Override
     public boolean delete(int id) throws Exception {
         Serie c = super.read(id);
-        //System.out.println("Deleting ParNomeID of id: " + id + " to Serie: " + c.getNome());
-        if(c != null) {
-            if(super.delete(id))
+        if (c != null) {
+            if (super.delete(id)) {
                 return indiceIndiretoNome.delete(ParNomeID.hash(c.getNome()));
+            }
         }
         return false;
     }
 
     @Override
     public boolean update(Serie novaSerie) throws Exception {
-        Serie SerieVelha = read(novaSerie.getNome());
-        if(super.update(novaSerie)) {
-            if(novaSerie.getNome().compareTo(SerieVelha.getNome())!=0) {
-                indiceIndiretoNome.delete(ParNomeID.hash(SerieVelha.getNome()));
+        Serie serieAntiga = read(novaSerie.getNome());
+        if (serieAntiga == null) {
+            System.out.println("Série não encontrada para atualização: " + novaSerie.getNome());
+            return false;
+        }
+        if (super.update(novaSerie)) {
+            if (!novaSerie.getNome().equals(serieAntiga.getNome())) {
+                indiceIndiretoNome.delete(ParNomeID.hash(serieAntiga.getNome()));
                 indiceIndiretoNome.create(new ParNomeID(novaSerie.getNome(), novaSerie.getId()));
             }
             return true;
