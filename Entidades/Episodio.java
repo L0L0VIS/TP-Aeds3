dde64aa4e3ff6b51110ff765dfb0e320483ddd1c
Mateus <166668055+L0L0VIS@users.noreply.github.com>
2025-04-04 16:54:55 -0300

    Update Episodio.java

--- Entidades/Episodio.java
+++ Entidades/Episodio.java
@@ -1,130 +1,94 @@
-package entidades;
+package Entidades;
 
+import java.io.*;
+import entidade.Registro;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+public class Episodio implements Registro {
 
-    public class Episodio implements Registro  {
+    private int id;               // Chave primária do episódio
+    private int idSerie;          // Chave estrangeira: ID da série a qual pertence
+    private String titulo;        // Título do episódio
+    private int temporada;        // Número da temporada
+    private int numero;           // Número do episódio
+    private String dataLancamento; // Data de lançamento (formato: "YYYY-MM-DD")
+    private int duracao;          // Duração do episódio em minutos
 
-    // Variáveis
-    private int id;                 // Chave
-    private int id_serie;           // Chave da série
-    private String nome;            // Nome do episódio
-    private short temporada;        // Temporada da série do episódio
-    private int data_lancamento;    // Data de lancamento (dia, mês e ano)
-    private short duracao;          // Duração em minutos do episódio
-
-    /* Constutores */
+    // Construtor padrão
     public Episodio() {
-        this(-1, -1, "", Short.valueOf("1"), -1, Short.valueOf("1"));
+        this(-1, -1, "", -1, -1, "0000-00-00", 0);
     }
 
-    public Episodio(int id, int id_serie, String nome, short temporada, int data_lancamento, short duracao) {
+    // Construtor completo
+    public Episodio(int id, int idSerie, String titulo, int temporada, int numero, 
+                    String dataLancamento, int duracao) {
         this.id = id;
-        this.id_serie = id_serie;
-        this.nome = nome;
+        this.idSerie = idSerie;
+        this.titulo = titulo;
         this.temporada = temporada;
-        this.data_lancamento = data_lancamento;
+        this.numero = numero;
+        this.dataLancamento = dataLancamento;
         this.duracao = duracao;
     }
 
-    public Episodio(int id_serie, String nome, short temporada, int data_lancamento, short duracao) {
-        this.id_serie = id_serie;
-        this.nome = nome;
-        this.temporada = temporada;
-        this.data_lancamento = data_lancamento;
-        this.duracao = duracao;
+    // Construtor sem ID (será gerado pelo sistema)
+    public Episodio(int idSerie, String titulo, int temporada, int numero, 
+                    String dataLancamento, int duracao) {
+        this(-1, idSerie, titulo, temporada, numero, dataLancamento, duracao);
     }
 
-    /* */
+    // Getters
+    public int getId() { return id; }
+    public int getIdSerie() { return idSerie; }
+    public String getTitulo() { return titulo; }
+    public int getTemporada() { return temporada; }
+    public int getNumero() { return numero; }
+    public String getDataLancamento() { return dataLancamento; }
+    public int getDuracao() { return duracao; }
 
-    /* Gets */
-    public int getId() {
-        return this.id;
-    }
-
-    public int getId_Serie() {
-        return this.id_serie;
-    }
-
-    public String getNome() {
-        return this.nome;
-    }
-
-    public short getTemporada() {
-        return this.temporada;
-    }
-
-    public int getData_lancamento() {
-        return this.data_lancamento;
-    }
-
-    public short getDuracao() {
-        return this.duracao;
-    }
-    /* */
-
-    /* Sets */
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setId_serie(int id_serie) {
-        this.id_serie = id_serie;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setTemporada(short temporada) {
-        this.temporada = temporada;
-    }
-
-    public void setData_lancamento(int data_lancamento) {
-        this.data_lancamento = data_lancamento;
-    }
-
-    public void setDuracao(short duracao) {
-        this.duracao = duracao;
-    }
-    /* */
+    // Setters
+    public void setId(int id) { this.id = id; }
+    public void setIdSerie(int idSerie) { this.idSerie = idSerie; }
+    public void setTitulo(String titulo) { this.titulo = titulo; }
+    public void setTemporada(int temporada) { this.temporada = temporada; }
+    public void setNumero(int numero) { this.numero = numero; }
+    public void setDataLancamento(String dataLancamento) { this.dataLancamento = dataLancamento; }
+    public void setDuracao(int duracao) { this.duracao = duracao; }
 
     @Override
     public int hashCode() {
-        return this.id;
+        return id;
     }
 
+    @Override
     public byte[] toByteArray() throws IOException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         DataOutputStream dos = new DataOutputStream(baos);
-        dos.writeInt(this.id);
-        dos.writeInt(this.id_serie);
-        dos.writeUTF(this.nome);
-        dos.writeShort(this.temporada);
-        dos.writeInt(this.data_lancamento);
-        dos.writeShort(this.duracao);
+        dos.writeInt(id);
+        dos.writeInt(idSerie);
+        dos.writeUTF(titulo);
+        dos.writeInt(temporada);
+        dos.writeInt(numero);
+        dos.writeUTF(dataLancamento);
+        dos.writeInt(duracao);
         return baos.toByteArray();
     }
 
+    @Override
     public void fromByteArray(byte[] ba) throws IOException {
         ByteArrayInputStream bais = new ByteArrayInputStream(ba);
         DataInputStream dis = new DataInputStream(bais);
-        this.id = dis.readInt();
-        this.id_serie = dis.readInt();
-        this.nome = dis.readUTF();
-        this.temporada = dis.readShort();
-        this.data_lancamento = dis.readInt();
-        this.duracao = dis.readShort();
+        id = dis.readInt();
+        idSerie = dis.readInt();
+        titulo = dis.readUTF();
+        temporada = dis.readInt();
+        numero = dis.readInt();
+        dataLancamento = dis.readUTF();
+        duracao = dis.readInt();
     }
 
-    // Print
+    @Override
     public String toString() {
-        return "(" + this.id + ";" + this.id_serie + ";" + this.nome + ";" +
-                this.temporada + ";" + this.data_lancamento + ";" + this.duracao + ")";
+        return "(" + id + "; Série " + idSerie + "; T: " + temporada + " Ep: " + numero +
+                "; Título: " + titulo + "; Lançamento: " + dataLancamento + "; Duração: " + duracao + " min)";
     }
-
 }
