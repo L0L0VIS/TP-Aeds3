041aec6efb58fe50072f745213e6ee76eab775a4
Mateus <166668055+L0L0VIS@users.noreply.github.com>
2025-04-04 16:56:15 -0300

    Update ArquivoEpisodio.java

--- arquivos/ArquivoEpisodio.java
+++ arquivos/ArquivoEpisodio.java
@@ -1,79 +1,68 @@
 package arquivos;
 
+import java.io.RandomAccessFile;
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
 
-import entidades.Episodio;
-import entidades.ParNomeID;
-import estruturas.HashExtensivel;
+import Entidades.Episodio;
+import entidade.ChaveComposta;
+import estruturas.ArvoreBMais;
 
-public class ArquivoEpisodio extends Arquivo<Episodio>
-{
+public class ArquivoEpisodio extends Arquivo<Episodio> {
 
-    Arquivo<Episodio> arqEpisodio;
-    HashExtensivel<ParNomeID> indiceIndiretoNome;
+    private ArvoreBMais<ChaveComposta> indiceBMais;
 
-    //@Override
     public ArquivoEpisodio() throws Exception {
-        super("Episodios", Episodio.class.getConstructor());
-        indiceIndiretoNome = new HashExtensivel<>(
-            ParNomeID.class.getConstructor(), 
-            4, 
-            ".\\dados\\Episodios\\indiceNome.d.db",   // diretório
-            ".\\dados\\Episodios\\indiceNome.c.db"    // cestos 
-        );
+        super("episodios", Episodio.class.getConstructor());
+
+        Constructor<ChaveComposta> c = ChaveComposta.class.getConstructor();
+        indiceBMais = new ArvoreBMais<>(c, 7, ".\\dados\\episodios\\indiceB+.db");
     }
 
     @Override
-    public int create(Episodio c) throws Exception {
-        //System.out.println("ArquivoEpisodio super being formed");
-        int id = super.create(c);
-        //System.out.println("ArquivoEpisodio super formed");
-        //System.out.println("Creating ParNomeID of id: " + id + " to Episodio: " + c.getNome());
-        //System.out.println("ArquivoEpisodio IndiceIndireto being formed");
-        indiceIndiretoNome.create(new ParNomeID(c.getNome(), id));
-        //System.out.println("ArquivoEpisodio IndiceIndireto formed");
+    public int create(Episodio ep) throws Exception {
+        int id = super.create(ep);
+        ChaveComposta chave = new ChaveComposta(ep.getIdSerie(), ep.getTemporada(), ep.getNumero());
+        indiceBMais.create(chave);
         return id;
     }
 
-    public Episodio read(String nome) throws Exception {
-        ParNomeID pni = indiceIndiretoNome.read(ParNomeID.hash(nome));
-        //System.out.println("PNomeID encontrado: " + pni.getId() + " " + pni.getnome());
-        if(pni == null)
-            return null;
-        return read(pni.getId());
+    public Episodio read(int idSerie, String titulo) throws Exception {
+        for (Episodio ep : listarTodos()) {
+            if (ep.getIdSerie() == idSerie && ep.getTitulo().equalsIgnoreCase(titulo)) {
+                return ep;
+            }
+        }
+        return null;
     }
     
-    public boolean delete(String nome) throws Exception {
-        ParNomeID pni = indiceIndiretoNome.read(ParNomeID.hash(nome));
-        //System.out.println("PNomeID encontrado para deletar: " + pni.getId() + " " + pni.getnome());
-        if(pni != null) 
-            if(delete(pni.getId())) 
-                //return indiceIndiretoNome.delete(ParNomeID.hash(nome));
-                return true;
-                // Antigo return substituído pois era redundante com o outro método delete
-        return false;
-    }
-
+    
     @Override
     public boolean delete(int id) throws Exception {
-        Episodio c = super.read(id);
-        //System.out.println("Deleting ParNomeID of id: " + id + " to Episodio: " + c.getNome());
-        if(c != null) {
-            if(super.delete(id))
-                return indiceIndiretoNome.delete(ParNomeID.hash(c.getNome()));
+        Episodio ep = super.read(id);
+        if (ep != null) {
+            ChaveComposta chave = new ChaveComposta(ep.getIdSerie(), ep.getTemporada(), ep.getNumero());
+            indiceBMais.delete(chave);
+            return super.delete(id);
         }
         return false;
     }
 
-    @Override
-    public boolean update(Episodio novaEpisodio) throws Exception {
-        Episodio EpisodioVelha = read(novaEpisodio.getNome());
-        if(super.update(novaEpisodio)) {
-            if(novaEpisodio.getNome().compareTo(EpisodioVelha.getNome())!=0) {
-                indiceIndiretoNome.delete(ParNomeID.hash(EpisodioVelha.getNome()));
-                indiceIndiretoNome.create(new ParNomeID(novaEpisodio.getNome(), novaEpisodio.getId()));
+    public ArrayList<Episodio> listarTodos() throws Exception {
+        ArrayList<Episodio> lista = new ArrayList<>();
+        int ultimoId = getUltimoID();
+        for (int i = 1; i <= ultimoId; i++) {
+            Episodio ep = super.read(i);
+            if (ep != null) {
+                lista.add(ep);
             }
-            return true;
         }
-        return false;
+        return lista;
+    }
+
+    private int getUltimoID() throws Exception {
+        try (RandomAccessFile raf = new RandomAccessFile(nomeArquivo, "r")) {
+            return raf.readInt();
+        }
     }
 }
